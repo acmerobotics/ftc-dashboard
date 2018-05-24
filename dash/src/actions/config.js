4363d1d8b204b860acb3e623dfb93df66cce4fa3
Ryan Brott <rcbrott@gmail.com>
2018-05-24 07:28:06 -0700

    Add support for partial configuration saves

--- dash/src/actions/config.js
+++ dash/src/actions/config.js
@@ -1,4 +1,4 @@
-// import { merge } from 'lodash';
+import { isPlainObject, isEqual, isEmpty } from 'lodash';
 
 export const RECEIVE_CONFIG = 'RECEIVE_CONFIG';
 export const GET_CONFIG = 'GET_CONFIG';
@@ -21,18 +21,39 @@ export const getConfig = () => ({
   type: GET_CONFIG
 });
 
-export const updateConfig = (modifiedOptions) => ({
+export const updateConfig = (options) => ({
   type: RECEIVE_MODIFIED_CONFIG,
-  data: modifiedOptions
+  data: options
 });
 
-export const saveConfig = () => (
+// XOR for sets
+const symmetricDifference = (a, b) => {
+  if (!a || !b) {
+    return a || b;
+  } else if (isPlainObject(a) || isPlainObject(b)) {
+    const diffObj = {};
+    Object.keys(a || b).forEach((key) => {
+      const diff = symmetricDifference(a[key], b[key]);
+      if (diff) {
+        diffObj[key] = diff;
+      }
+    });
+    return isEmpty(diffObj) ? undefined : diffObj;
+  } else {
+    return isEqual(a, b) ? undefined : a || b;
+  }
+};
+
+export const saveConfig = (options) => (
   (dispatch, getState) => {
+    const { modifiedOptions } = getState().config;
+    const optionsToSave = options || modifiedOptions;
+
     dispatch({
       type: SAVE_CONFIG,
-      data: getState().config.modifiedOptions
+      data: optionsToSave
     });
 
-    dispatch(updateConfig({}));
+    dispatch(updateConfig(symmetricDifference(modifiedOptions, optionsToSave) || {}));
   }
 );
