8fa85caf7afae1df1fe3d90026768fa9803f164b
Ryan Brott <rcbrott@gmail.com>
2018-05-24 21:26:36 -0700

    Add opmode init/start/stop support

--- dash/src/actions/status.js
+++ dash/src/actions/status.js
@@ -1,6 +1,5 @@
 export const GET_ROBOT_STATUS = 'GET_ROBOT_STATUS';
 export const RECEIVE_ROBOT_STATUS = 'RECEIVE_ROBOT_STATUS';
-export const GET_OP_MODE_LIST = 'GET_OP_MODE_LIST';
 export const RECEIVE_OP_MODE_LIST = 'RECEIVE_OP_MODE_LIST';
 
 export const getRobotStatus = () => ({
@@ -10,10 +9,6 @@ export const getRobotStatus = () => ({
 export const receiveRobotStatus = (data) => ({
   type: RECEIVE_ROBOT_STATUS,
   data
-});
-
-export const getOpModeList = () => ({
-  type: GET_OP_MODE_LIST
 });
 
 export const receiveOpModeList = (data) => ({
