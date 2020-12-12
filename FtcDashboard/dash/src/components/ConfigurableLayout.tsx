401a200047c6b694641dffc844afee67d9211dd7
Noah Bresler <noahbresler2002@gmail.com>
2020-12-12 10:05:30 -0600

    Add grid view map with uuids

--- FtcDashboard/dash/src/components/ConfigurableLayout.tsx
+++ FtcDashboard/dash/src/components/ConfigurableLayout.tsx
@@ -1,11 +1,27 @@
-import { Responsive, WidthProvider, Layout } from 'react-grid-layout';
+import React, { ReactElement, useState } from 'react';
+import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import { v4 as uuidv4 } from 'uuid';
 
 import GraphView from '../containers/GraphView';
 import FieldView from '../containers/FieldView';
 import ConfigView from '../containers/ConfigView';
 import TelemetryView from '../containers/TelemetryView';
+
+enum SupportedViews {
+  FIELD_VIEW,
+  GRAPH_VIEW,
+  CONFIG_VIEW,
+  TELEMETRY_VIEW,
+}
+
+const ViewMap: { [key in SupportedViews]: ReactElement } = {
+  [SupportedViews.FIELD_VIEW]: <FieldView />,
+  [SupportedViews.GRAPH_VIEW]: <GraphView />,
+  [SupportedViews.CONFIG_VIEW]: <ConfigView />,
+  [SupportedViews.TELEMETRY_VIEW]: <TelemetryView />,
+};
 
 export default function ConfigurableLayout() {
   const ResponsiveReactGridLayout = WidthProvider(Responsive);
@@ -18,34 +34,50 @@ export default function ConfigurableLayout() {
     xxs: 1,
   };
 
-  const defaultLayout: Layout[] = [
-    { i: 'a', x: 0, y: 0, w: 1, h: 6 },
-    { i: 'b', x: 1, y: 0, w: 1, h: 6 },
-    { i: 'c', x: 2, y: 0, w: 1, h: 6 },
-    { i: 'd', x: 2, y: 6, w: 1, h: 2 },
+  const defaultGrid = [
+    {
+      id: uuidv4(),
+      view: SupportedViews.FIELD_VIEW,
+      layout: { x: 0, y: 0, w: 1, h: 6 },
+    },
+    {
+      id: uuidv4(),
+      view: SupportedViews.GRAPH_VIEW,
+      layout: { x: 1, y: 0, w: 1, h: 6 },
+    },
+    {
+      id: uuidv4(),
+      view: SupportedViews.CONFIG_VIEW,
+      layout: { x: 2, y: 0, w: 1, h: 6 },
+    },
+    {
+      id: uuidv4(),
+      view: SupportedViews.TELEMETRY_VIEW,
+      layout: { x: 2, y: 6, w: 1, h: 2 },
+    },
   ];
+
+  const [gridItems] = useState(defaultGrid);
 
   return (
     <ResponsiveReactGridLayout
       className="layout"
       cols={colBreakpoints}
-      layouts={{ lg: defaultLayout }}
-      resizeHandles={['se']}
+      layouts={{
+        lg: defaultGrid.map((x) => Object.assign(x.layout, { i: x.id })),
+      }}
+      resizeHandles={['ne', 'nw', 'se', 'sw']}
       draggableHandle=".grab-handle"
-      verticalCompact={false}
+      compactType={null}
     >
-      <div key="a">
-        <FieldView />
-      </div>
-      <div key="b">
-        <GraphView />
-      </div>
-      <div key="c">
-        <ConfigView />
-      </div>
-      <div key="d">
-        <TelemetryView />
-      </div>
+      {gridItems.map((item) => (
+        <div key={item.id}>
+          {ViewMap[item.view]}
+          {/* {React.cloneElement(ViewMap[item.view], {
+            configurableGridId: item.id,
+          })} */}
+        </div>
+      ))}
     </ResponsiveReactGridLayout>
   );
 }
