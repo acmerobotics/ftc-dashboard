bee5bcc398b3e3c9dd1204dc339ef2c35e4c666f
Noah Bresler <noahbresler2002@gmail.com>
2020-12-21 05:49:19 -0600

    Add stylewind css linter dependency
    Default vscode linter did not like the tailwind apply function

---
+++ FtcDashboard/dash/stylelint.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  rules: {
+    'at-rule-empty-line-before': [
+      'always',
+      {
+        except: [
+          'blockless-after-blockless',
+          'blockless-after-same-name-blockless',
+          'first-nested',
+        ],
+        ignore: ['after-comment', 'inside-block'],
+        ignoreAtRules: ['apply', 'screen', 'font-face', 'nest'],
+      },
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'variants',
+          'responsive',
+          'apply',
+          'screen',
+        ],
+      },
+    ],
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['font-path'],
+      },
+    ],
+    'selector-nested-pattern': '^&',
+  },
+};
