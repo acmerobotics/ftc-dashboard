a4237e29f3465be89fa71f8937029b0f9339f80f
Noah Bresler <noahbresler2002@gmail.com>
2020-12-26 19:55:02 -0600

    Background dot grid resizes to align with the actual grid

--- FtcDashboard/dash/src/components/ConfigurableLayout.tsx
+++ FtcDashboard/dash/src/components/ConfigurableLayout.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement, useState, useEffect, useRef } from 'react';
 import RGL, { WidthProvider, Layout } from 'react-grid-layout';
 import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
 
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import styled from 'styled-components';
 
 import { ConfigurableView } from '../enums/ConfigurableView';
 import GraphView from '../containers/GraphView';
@@ -62,6 +62,7 @@ const GRID_COL = 12;
 const GRID_ROW_HEIGHT = 60;
 const GRID_MARGIN = 10;
 const GRID_ITEM_MIN_WIDTH = 3;
+const GRID_DOT_PADDING = 10;
 
 const ReactGridLayout = WidthProvider(RGL);
 
@@ -71,7 +72,7 @@ const Container = styled.div.attrs<{ isLayoutLocked: boolean }>(
       !isLayoutLocked ? 'bg-gray-100' : 'bg-white'
     } p-2 transition-colors`,
   }),
-)<{ isLayoutLocked: boolean }>`
+)<{ isLayoutLocked: boolean; bgGridSize: number }>`
   position: relative;
 
   height: calc(100vh - 52px);
@@ -80,11 +81,12 @@ const Container = styled.div.attrs<{ isLayoutLocked: boolean }>(
   overflow-y: scroll;
   padding-bottom: 1em;
 
-  ${({ isLayoutLocked }) =>
-    !isLayoutLocked
-      ? 'background-image: radial-gradient(#d2d2d2 5%, transparent 0);'
-      : ''}
-  background-size: 35px 35px;
+  background-image: ${(props) =>
+    !props.isLayoutLocked
+      ? `radial-gradient(#94a3b8 calc((0.5rem + ${GRID_DOT_PADDING}px) - 17px), transparent 0)`
+      : ''};
+  background-size: ${(props) => `${props.bgGridSize}px  ${props.bgGridSize}px`};
+  background-position: ${`calc(0.5rem + ${GRID_DOT_PADDING}px) calc(0.5rem + ${GRID_DOT_PADDING}px - 5px)`};
 `;
 
 interface GridItem {
@@ -275,10 +277,13 @@ const DEFAULT_GRID_TALL: GridItem[] = [
 
 export default function ConfigurableLayout() {
   const containerRef = useRef<HTMLDivElement>(null);
+  const gridWrapperRef = useRef<HTMLDivElement>(null);
 
   const [isLayoutLocked, setIsLayoutLocked] = useState(true);
   const [isInDeleteMode, setIsInDeleteMode] = useState(false);
   const [isShowingViewPicker, setIsShowingViewPicker] = useState(false);
+
+  const [gridBgSize, setGridBgSize] = useState(40);
 
   const [
     gridItems,
@@ -296,6 +301,27 @@ export default function ConfigurableLayout() {
     width: '14em',
     height: '13em',
   });
+
+  useEffect(() => {
+    const containerResizerObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        if (entry.target === gridWrapperRef.current) {
+          const width =
+            gridWrapperRef.current.clientWidth - 2 * GRID_DOT_PADDING;
+          setGridBgSize(
+            width / ((GRID_COL / 4) * Math.floor(width / 300) + 1) / 3,
+          );
+        }
+      }
+    });
+
+    if (gridWrapperRef.current !== null)
+      containerResizerObserver.observe(gridWrapperRef.current);
+
+    return () => {
+      containerResizerObserver.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
     const initialLayoutStorageValue = window.localStorage.getItem(
@@ -470,7 +496,11 @@ export default function ConfigurableLayout() {
   };
 
   return (
-    <Container ref={containerRef} isLayoutLocked={isLayoutLocked}>
+    <Container
+      ref={containerRef}
+      isLayoutLocked={isLayoutLocked}
+      bgGridSize={gridBgSize}
+    >
       {gridItems.length === 0 ? (
         <div
           className={`text-center mt-16 p-12 transition-colors ${
@@ -487,45 +517,47 @@ export default function ConfigurableLayout() {
       ) : (
         ''
       )}
-      <ReactGridLayout
-        className="layout"
-        cols={GRID_COL}
-        resizeHandles={['se']}
-        draggableHandle=".grab-handle"
-        compactType={null}
-        rowHeight={
-          isLayoutLocked ? GRID_ROW_HEIGHT + GRID_MARGIN : GRID_ROW_HEIGHT
-        }
-        layout={gridItems.map((item) => ({ i: item.id, ...item.layout }))}
-        onLayoutChange={onLayoutChange}
-        margin={isLayoutLocked ? [0, 0] : [GRID_MARGIN, GRID_MARGIN]}
-      >
-        {gridItems.map((item) => (
-          <div key={item.id}>
-            {React.cloneElement(VIEW_MAP[item.view], {
-              isDraggable: item.layout.isDraggable,
-              isUnlocked: !isLayoutLocked,
-            })}
-            <div
-              className={`absolute top-0 left-0 w-full h-full bg-yellow-300 bg-opacity-50 flex-center rounded transition ${
-                isInDeleteMode
-                  ? 'pointer-events opacity-100'
-                  : 'pointer-events-none opacity-0'
-              }`}
-            >
-              <button
-                className="p-4 border-4 border-yellow-600 rounded-full bg-opacity-50 opacity-50 focus:outline-none focus:ring focus:ring-yellow-800"
-                onClick={() => {
-                  removeItem(item.id);
-                }}
-                disabled={!isInDeleteMode}
+      <div ref={gridWrapperRef}>
+        <ReactGridLayout
+          className="layout"
+          cols={GRID_COL}
+          resizeHandles={['se']}
+          draggableHandle=".grab-handle"
+          compactType={null}
+          rowHeight={
+            isLayoutLocked ? GRID_ROW_HEIGHT + GRID_MARGIN : GRID_ROW_HEIGHT
+          }
+          layout={gridItems.map((item) => ({ i: item.id, ...item.layout }))}
+          onLayoutChange={onLayoutChange}
+          margin={isLayoutLocked ? [0, 0] : [GRID_MARGIN, GRID_MARGIN]}
+        >
+          {gridItems.map((item) => (
+            <div key={item.id}>
+              {React.cloneElement(VIEW_MAP[item.view], {
+                isDraggable: item.layout.isDraggable,
+                isUnlocked: !isLayoutLocked,
+              })}
+              <div
+                className={`absolute top-0 left-0 w-full h-full bg-yellow-300 bg-opacity-50 flex-center rounded transition ${
+                  isInDeleteMode
+                    ? 'pointer-events opacity-100'
+                    : 'pointer-events-none opacity-0'
+                }`}
               >
-                <DeleteXSVG className="text-yellow-600 w-20 h-20" />
-              </button>
+                <button
+                  className="p-4 border-4 border-yellow-600 rounded-full bg-opacity-50 opacity-50 focus:outline-none focus:ring focus:ring-yellow-800"
+                  onClick={() => {
+                    removeItem(item.id);
+                  }}
+                  disabled={!isInDeleteMode}
+                >
+                  <DeleteXSVG className="text-yellow-600 w-20 h-20" />
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-      </ReactGridLayout>
+          ))}
+        </ReactGridLayout>
+      </div>
       <RadialFab
         width="4em"
         height="4em"
