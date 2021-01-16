e36c99b5b22589696b505db1c96c21a78bb5ba33
Noah Bresler <noahbresler2002@gmail.com>
2021-01-16 12:50:29 -0600

    Implement SAVE_LAYOUT_PRESET in middleware

--- FtcDashboard/dash/src/store/middleware/storageMiddleware.ts
+++ FtcDashboard/dash/src/store/middleware/storageMiddleware.ts
@@ -1,7 +1,7 @@
 import { Middleware } from 'redux';
 
 import LayoutPreset, { LayoutPresetType } from '../../enums/LayoutPreset';
-import { GET_LAYOUT_PRESET } from '../types';
+import { GET_LAYOUT_PRESET, SAVE_LAYOUT_PRESET } from '../types';
 import { receiveLayoutPreset } from '../actions/settings';
 import { RootState } from '../reducers';
 
@@ -16,6 +16,13 @@ const storageMiddleware: Middleware<Record<string, unknown>, RootState> = (
         localStorage.getItem(LAYOUT_PRESET_KEY) || LayoutPreset.DEFAULT;
 
       store.dispatch(receiveLayoutPreset(preset as LayoutPresetType));
+
+      break;
+    }
+    case SAVE_LAYOUT_PRESET: {
+      localStorage.setItem(LAYOUT_PRESET_KEY, action.preset);
+
+      store.dispatch(receiveLayoutPreset(action.preset));
 
       break;
     }
