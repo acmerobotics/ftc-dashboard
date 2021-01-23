9b61792fb61e48320fc9ef3cc2fcaeadd6861791
Noah Bresler <noahbresler2002@gmail.com>
2021-01-23 16:52:29 -0600

    Move stop tag to opmode types

--- FtcDashboard/dash/src/store/types/opmode.ts
+++ FtcDashboard/dash/src/store/types/opmode.ts
@@ -2,6 +2,8 @@ export const INIT_OP_MODE = 'INIT_OP_MODE';
 export const START_OP_MODE = 'START_OP_MODE';
 export const STOP_OP_MODE = 'STOP_OP_MODE';
 
+export const STOP_OP_MODE_TAG = '$Stop$Robot$';
+
 export type InitOpModeAction = {
   type: typeof INIT_OP_MODE;
   opModeName: string;
