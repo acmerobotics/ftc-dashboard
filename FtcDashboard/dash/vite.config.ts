91086f036e55e0be78821762d943ee17e01904be
Noah Bresler <noahbresler2002@gmail.com>
2022-09-24 21:10:40 -0400

    Bump deps

--- FtcDashboard/dash/vite.config.ts
+++ FtcDashboard/dash/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dash',
-  plugins: [reactRefresh(), svgr()],
+  base: '/dash/',
+  plugins: [react(), svgr()],
 });
