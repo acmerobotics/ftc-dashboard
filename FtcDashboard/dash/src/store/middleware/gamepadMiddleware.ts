a6412d468ecccc460ed6793ab48f6bb7e543d007
Maftei Albert-Alexandru <maftei.albert.alexandru@gmail.com>
2022-09-26 21:01:41 +0300

    Store the object in a variable and then conditionally add touchpad + explanation

--- FtcDashboard/dash/src/store/middleware/gamepadMiddleware.ts
+++ FtcDashboard/dash/src/store/middleware/gamepadMiddleware.ts
@@ -144,39 +144,8 @@ const extractGamepadState = (gamepad: Gamepad) => {
         right_trigger: gamepad.buttons[7].value,
       };
     case GamepadType.SONY_DUALSHOCK_4:
-      if (gamepad.buttons[17] !== undefined)
-        return {
-          left_stick_x: cleanMotionValues(gamepad.axes[0]),
-          left_stick_y: cleanMotionValues(gamepad.axes[1]),
-          right_stick_x: cleanMotionValues(gamepad.axes[2]),
-          right_stick_y: cleanMotionValues(gamepad.axes[3]),
-
-          dpad_up: gamepad.buttons[12].pressed,
-          dpad_down: gamepad.buttons[13].pressed,
-          dpad_left: gamepad.buttons[14].pressed,
-          dpad_right: gamepad.buttons[15].pressed,
-
-          a: gamepad.buttons[0].pressed,
-          b: gamepad.buttons[1].pressed,
-          x: gamepad.buttons[2].pressed,
-          y: gamepad.buttons[3].pressed,
-
-          guide: gamepad.buttons[16].pressed,
-          start: gamepad.buttons[9].pressed,
-          back: gamepad.buttons[8].pressed,
-
-          left_bumper: gamepad.buttons[4].pressed,
-          right_bumper: gamepad.buttons[5].pressed,
-
-          left_stick_button: gamepad.buttons[10].pressed,
-          right_stick_button: gamepad.buttons[11].pressed,
-          left_trigger: gamepad.buttons[6].value,
-          right_trigger: gamepad.buttons[7].value,
-
-          touchpad: gamepad.buttons[17].pressed,
-        };
-      else
-        return {
+      {
+        const state: GamepadState = {
           left_stick_x: cleanMotionValues(gamepad.axes[0]),
           left_stick_y: cleanMotionValues(gamepad.axes[1]),
           right_stick_x: cleanMotionValues(gamepad.axes[2]),
@@ -204,6 +173,12 @@ const extractGamepadState = (gamepad: Gamepad) => {
           left_trigger: gamepad.buttons[6].value,
           right_trigger: gamepad.buttons[7].value,
         };
+        if (gamepad.buttons[17] !== undefined) {
+          state.touchpad = gamepad.buttons[17].pressed;
+        }
+        return state;
+      }
+      break;
 
     default:
       throw new Error(`Unable to handle support gamepad of type ${type}`);
