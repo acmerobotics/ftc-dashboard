58c49b114f3410206e54cf151627c915e8982b69
Noah Bresler <noahbresler2002@gmail.com>
2022-11-21 22:09:52 -0500

    Move DeveloperModeDialog into own component

---
+++ FtcDashboard/dash/src/components/DeveloperModeDialog.tsx
@@ -0,0 +1,81 @@
+import { Dialog, Switch, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
+
+export default function DeveloperModeDialog({
+  isOpen,
+  onClose,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  const [mockSocketEnabled, setMockSocketEnabled] = useState(false);
+
+  return (
+    <Transition as={Fragment} show={isOpen}>
+      <Dialog onClose={onClose}>
+        {/* Backdrop */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-150"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-full p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-150"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-100"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md py-6 overflow-hidden text-left align-middle transition-all transform bg-white rounded-md shadow-xl">
+                <Dialog.Title className="px-6 mb-4 text-xl font-medium">
+                  Developer Options
+                </Dialog.Title>
+                <ul>
+                  <li
+                    className={`px-4 ${
+                      !mockSocketEnabled ? 'bg-gray-100' : ''
+                    }`}
+                  >
+                    <section className="px-2 border-t border-b border-gray-300">
+                      <div className="flex flex-row items-center justify-between">
+                        <h4 className="py-4 font-medium">Mock Socket</h4>
+                        <Switch
+                          checked={mockSocketEnabled}
+                          onChange={setMockSocketEnabled}
+                          className={`${
+                            mockSocketEnabled
+                              ? 'bg-blue-600 border-blue-700'
+                              : 'bg-gray-300 border-gray-400'
+                          } relative inline-flex h-6 w-11 items-center rounded-full border`}
+                        >
+                          <span
+                            className={`${
+                              mockSocketEnabled
+                                ? 'translate-x-[1.35rem]'
+                                : 'translate-x-1'
+                            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                          />
+                        </Switch>
+                      </div>
+                    </section>
+                  </li>
+                </ul>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
--- FtcDashboard/dash/src/containers/Dashboard.tsx
+++ FtcDashboard/dash/src/containers/Dashboard.tsx
@@ -1,96 +1,19 @@
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import LayoutPreset, { LayoutPresetType } from '../enums/LayoutPreset';
 import { connect, disconnect } from '../store/actions/socket';
 import { saveLayoutPreset, getLayoutPreset } from '../store/actions/settings';
+import { RootState } from '../store/reducers';
 
 import { ReactComponent as ConnectedIcon } from '../assets/icons/wifi.svg';
 import { ReactComponent as DisconnectedIcon } from '../assets/icons/wifi_off.svg';
 import { ReactComponent as DeveloperModeIcon } from '../assets/icons/developer_mode.svg';
-import { RootState } from '../store/reducers';
-import { Dialog, Switch, Transition } from '@headlessui/react';
+
 import useDelayedTooltip from '../hooks/useDelayedTooltip';
 import ToolTip from '../components/ToolTip';
 
-const DeveloperModeDialog = ({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
-  const [mockSocketEnabled, setMockSocketEnabled] = useState(false);
-
-  return (
-    <Transition as={Fragment} show={isOpen}>
-      <Dialog onClose={onClose}>
-        {/* Backdrop */}
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-150"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black/30" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-full p-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-150"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-100"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-md py-6 overflow-hidden text-left align-middle transition-all transform bg-white rounded-md shadow-xl">
-                <Dialog.Title className="px-6 mb-4 text-xl font-medium">
-                  Developer Options
-                </Dialog.Title>
-                <ul>
-                  <li
-                    className={`px-4 ${
-                      !mockSocketEnabled ? 'bg-gray-100' : ''
-                    }`}
-                  >
-                    <section className="px-2 border-t border-b border-gray-300">
-                      <div className="flex flex-row items-center justify-between">
-                        <h4 className="py-4 font-medium">Mock Socket</h4>
-                        <Switch
-                          checked={mockSocketEnabled}
-                          onChange={setMockSocketEnabled}
-                          className={`${
-                            mockSocketEnabled
-                              ? 'bg-blue-600 border-blue-700'
-                              : 'bg-gray-300 border-gray-400'
-                          } relative inline-flex h-6 w-11 items-center rounded-full border`}
-                        >
-                          <span
-                            className={`${
-                              mockSocketEnabled
-                                ? 'translate-x-[1.35rem]'
-                                : 'translate-x-1'
-                            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-                          />
-                        </Switch>
-                      </div>
-                    </section>
-                  </li>
-                </ul>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition>
-  );
-};
+import DeveloperModeDialog from '../components/DeveloperModeDialog';
 
 export default function Dashboard() {
   const dispatch = useDispatch();
