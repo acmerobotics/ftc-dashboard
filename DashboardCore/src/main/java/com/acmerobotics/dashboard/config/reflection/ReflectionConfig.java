0191cfdcac908480434d0eb4f1181a2c29d330fe
Maftei Albert-Alexandru <maftei.albert.alexandru@gmail.com>
2023-11-05 01:56:22 +0200

    Add array config support (#144)
    Add support for using fixed-size arrays in configuration variables.

--- DashboardCore/src/main/java/com/acmerobotics/dashboard/config/reflection/ReflectionConfig.java
+++ DashboardCore/src/main/java/com/acmerobotics/dashboard/config/reflection/ReflectionConfig.java
@@ -5,8 +5,10 @@ import com.acmerobotics.dashboard.config.variable.ConfigVariable;
 import com.acmerobotics.dashboard.config.variable.CustomVariable;
 import com.acmerobotics.dashboard.config.variable.VariableType;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class ReflectionConfig {
     private ReflectionConfig() {}
@@ -24,7 +26,60 @@ public class ReflectionConfig {
 
         return customVariable;
     }
+    private static ConfigVariable<?>  createVariableFromArrayField(Field field, Class<?> fieldClass, Object parent, int[] indices) {
+        VariableType type = VariableType.fromClass(fieldClass);
+        switch (type) {
+            case BOOLEAN:
+            case INT:
+            case DOUBLE:
+            case STRING:
+            case ENUM:
+                return new BasicVariable<>(type, new ArrayProvider<Boolean>(field, parent,Arrays.copyOf(indices, indices.length)));
+            case CUSTOM:
+                try {
+                    Object value = null;
+                    try {
+                        value = ArrayProvider.getArrayRecursive(field.get(parent), indices);
+                    }catch (ArrayIndexOutOfBoundsException ignored )
+                    {
 
+                    }
+
+                    if (value == null) {
+                        return new CustomVariable(null);
+                    }
+                    CustomVariable customVariable = new CustomVariable();
+                    if(fieldClass.isArray())
+                    {
+                        int[] newIndices = Arrays.copyOf(indices, indices.length+1);
+
+                        for(int i = 0; i<Array.getLength(value); i++)
+                        {
+
+                            newIndices[newIndices.length-1]=i;
+                            customVariable.putVariable(Integer.toString(i), createVariableFromArrayField(field, fieldClass.getComponentType(), parent, newIndices));
+                        }
+                    }
+                    else {
+                        for (Field nestedField : fieldClass.getFields()) {
+                            if (Modifier.isFinal(field.getModifiers())) {
+                                continue;
+                            }
+
+                            String name = nestedField.getName();
+                            customVariable.putVariable(name,
+                                    createVariableFromField(nestedField, value));
+                        }
+                    }
+                    return customVariable;
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            default:
+                throw new RuntimeException("Unsupported field type: " +
+                        fieldClass.getName());
+        }
+    }
     private static ConfigVariable<?> createVariableFromField(Field field, Object parent) {
         Class<?> fieldClass = field.getType();
         VariableType type = VariableType.fromClass(fieldClass);
@@ -41,18 +96,25 @@ public class ReflectionConfig {
                     if (value == null) {
                         return new CustomVariable(null);
                     }
-
                     CustomVariable customVariable = new CustomVariable();
-                    for (Field nestedField : fieldClass.getFields()) {
-                        if (Modifier.isFinal(field.getModifiers())) {
-                            continue;
+                    if(fieldClass.isArray())
+                    {
+                        for(int i = 0; i<Array.getLength(value); i++)
+                        {
+                            customVariable.putVariable(Integer.toString(i), createVariableFromArrayField(field, field.getType().getComponentType(), parent, new int[]{i}));
                         }
-
-                        String name = nestedField.getName();
-                        customVariable.putVariable(name,
-                                createVariableFromField(nestedField, value));
                     }
+                    else {
+                        for (Field nestedField : fieldClass.getFields()) {
+                            if (Modifier.isFinal(field.getModifiers())) {
+                                continue;
+                            }
 
+                            String name = nestedField.getName();
+                            customVariable.putVariable(name,
+                                    createVariableFromField(nestedField, value));
+                        }
+                    }
                     return customVariable;
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
